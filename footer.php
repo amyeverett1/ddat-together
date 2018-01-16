378b123b09f79fb64a0a4781e71aa46c7a60838f
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-01-16 17:13:55 +0000

    all files commit

---
+++ footer.php
@@ -0,0 +1,21 @@
+        <footer>
+            <p>For the best experience using this site, we recommend you use Google Chrome as your browser.</p>
+               <div class="footer_button_container">
+       <a class="btTxt" href="https://ddat-together-events.herokuapp.com/"><i class="fa fa-angle-double-left fa-lg" aria-hidden="true"></i> Back to the main DDaT events site</a>
+            </div>   
+        </footer>
+
+    <script src="lib/jquery-2.0.3.js"></script>
+<script src="src/js/jquery.swipebox.js"></script>
+
+<script type="text/javascript">
+;( function( $ ) {
+
+	$( '.swipebox' ).swipebox();
+
+} )( jQuery );
+</script>
+
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,329 @@
+<?php include('header.php') ?>
+    <h1>DDaT Together</h1>
+</header>
+
+<div class="date">
+    <span>Tuesday 3 October 2017 <br> Alec Stewart Gate, Harleyford Road, The Kia Oval, Kennington, London SE11 5SS</span>
+</div>
+
+<div class="container index"> 
+ 
+    <div class="accordion">
+       
+<h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>Agenda</span></h3> 
+<div>
+<div class="wrapper">
+
+    
+<div class="box white agenda">
+ 
+    
+
+    
+<div class="agenda-time">
+    <span class="time">09.30 – 10.30</span>
+    <span>Registration</span>
+    <span class="list">DDaT market stalls running during registration</span>
+</div>
+
+<div class="agenda-time">
+    <span class="time">10.30 – 11.00</span>
+    <span>Welcome and introduction  - Pete Rose, CDDaTO</span>
+</div>
+
+<div class="agenda-time">
+    <span class="time">11.00 – 11.30</span>
+    <span>Keynote speaker - Jacqueline de Rojas, President techUK</span>
+</div>
+      
+<div class="agenda-time">
+    <span class="time">11.30 – 11.40</span>
+    <span>Questions &amp; Answers – DDaT Management Team</span>
+</div>
+    
+<div class="agenda-time">
+    <span class="time">11.40 – 11.55</span>
+    <span>Comfort break</span>
+</div>
+          
+<div class="agenda-time">
+    <span class="time">11.55 – 12.40</span>
+    <span>Vision/Mission/Objectives/Strategy/Tactics (VMOST) - Pete Rose</span>
+</div>
+           
+<div class="agenda-time">
+    <span class="time">12.40 – 13.30</span>
+    <span>Lunch</span>
+</div>
+        
+<div class="agenda-time">
+    <span class="time">13.30 – 14.00</span>
+    <span>Lightning talks – DDaT volunteers</span>
+</div>
+ 
+<div class="agenda-time">
+    <span class="time">14.00 - 14.05 </span>
+    <span>Afternoon sessions: Introduction – MCs (John Holland &amp; Margaret-Mary Wilmot)</span>
+</div>
+
+<div class="agenda-time">
+    <span class="time">14.05 - 15.10</span>
+    <span>Breakout Sessions</span>
+    <span><strong>Group 1:</strong> Keynote speaker – Tom Read, Chief Digital and Information Officer, Ministry of Justice (England Suite)</span>
+    <span><strong>Group 2:</strong> Collaboration, Convergence and Inclusion breakouts: HO Organisation Design Team (4 x breakout sessions)</span>
+</div>
+  
+<div class="agenda-time">
+    <span class="time">15.10 – 16.15</span>
+    <span>Breakout Sessions</span>
+    <span><strong>Group 1</strong>: Collaboration, Convergence and Inclusion breakouts: HO Organisation Design Team (4 x breakout sessions)</span>
+    <span><strong>Group 2:</strong> Keynote speaker – Tom Read, Chief Digital and Information Officer, Ministry of Justice (England Suite)</span>
+</div> 
+ 
+ <div class="agenda-time">
+    <span class="time">16.15 – 16.30</span>
+    <span>Conclusions and next steps – Pete Rose/Management Team/Claire Fisher/Tom Read</span>
+</div>
+    
+ <div class="agenda-time">
+    <span class="time">16.30</span>
+    <span>Summary and close - Pete Rose, CDDaTO</span>
+</div>
+    
+
+         
+    </div>
+
+    </div> <!-- wrapper-->
+    
+  </div><!-- end agenda-->
+    
+
+  <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>Keynote Speakers</span></h3>
+  <div>
+
+         <div class="wrapper">
+            <div class="box white">
+<h3><a href="https://twitter.com/JdR_Tech">Jacqueline de Rojas</a></h3>
+    
+<div class="text">
+    <p>Jacqueline is the President of techUK and sits as a Non-Executive Director on the board of UK technology business Rightmove plc. Serving as a member of the government's Digital Economy Council, Jacqueline is also a mentor to boards and executives through Bird &amp; Co.</p>
+
+ <p>Her presidency focuses on the ambition for the UK to seize its position as a digital nation of significance by leveraging its digital opportunity and skills. She believes that to achieve this all geographies and demographics across the UK must be included and we must strive to equip our country with the very best infrastructure and supportive business legislation possible. Above all, none of the above can be achieved unless the UK tech industry demonstrates that it is open for business to global markets.</p>
+
+ <p>Prior to her presidency, Jacqueline held the role of deputy president techUK and board champion for women. She has used this role to leverage resources in the technology industry to support her manifesto to increase diversity of skills and talent in the tech industry. This has gathered increasing momentum through techUK's Women in Tech Council, which continues to bring industry together to drive for inclusion, diversity &amp; gender equality.</p>
+
+ <p>Jacqueline has been employed throughout her career by global blue-chip software companies to accelerate growth by pulling amazing teams together which can operate under pressure and extend their reach through smart partnerships.</p> 
+
+ <p>Jacqueline was previously a Non-Executive Director at Home Retail Group plc; is currently a board advisor to Digital Leaders, Accelerate-HER and lends her support to everywoman. This year, Jacqueline was listed as one of Europe's Inspiring Fifty Role Models. In 2016 she was listed on Debretts 2016 500 Digital People of Influence and entered the @Computerweekly Hall of Fame as the most influential woman in UK IT; she was voted Computer Weekly's Most Influential Woman in UK IT in 2015. She was recently appointed as a special advisor to the Girlguiding Association for technology transformation.</p>
+          <p class="small">Content credit to <a href="http://www.techuk.org/about/our-board/item/222-jacqueline-de-rojas">tech.uk</a></p>
+</div>
+                
+            <div class="image_box">
+                <img class="image_display" src="/img/jac.jpg" alt="photograph of Jacqueline de Rojas"/>     
+            </div>
+                
+        
+           </div><!-- boxwhite-->
+             
+             
+             
+               <div class="box white">
+             
+             <h3><a href="https://twitter.com/thommeread">Tom Read</a></h3>
+    
+<div class="text">
+             <p>Tom Read is currently Chief Digital and Information Officer at Ministry of Justice. He says: My career has focused on technology as an agent of change and growth. In short, how organisations can bring digital into their core to make things better, faster and more profitable.</p>
+
+<p>I have worked across banking, media, and most recently government as a Deputy Director at the Government Digital Service and as CTO at the Cabinet Office.</p>
+
+<p>My successes so far have been built on a few key beliefs. Firstly that everything we do should start with the user: from deciding what laptop to provision to designing a digital service, the user needs to be at the centre of every decision. Secondly that people can do amazing things and will love change if you include them in every decision. Thirdly, that things don't need to be nearly this complicated in the digital and technology space: start small, iterate and use simple language to describe what you're doing. </p>
+             <p class="small">Content credit to <a href="https://www.linkedin.com/in/thommeread/">linkedin.com</a></p>
+                   </div>
+                   
+                   <div class="image_box">
+                <img class="image_display" src="/img/tom.jpg" alt="photograph of Tom Read"/>     
+            </div>
+                   
+             </div><!--box white-->
+             
+        </div> <!-- wrapper-->    
+      
+  </div> <!--end presenters--> 
+        
+        
+   <h3><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <span>Register to Attend</span></h3>
+  <div>
+   
+   <div class="wrapper">    
+    <div class="box white">      
+        
+        <div id="eventzilla-iframe"></div><script type='text/javascript'>window.onload = function() {var iframe = document.createElement('iframe');iframe.id="ifeventzilla";iframe.style.width = "100%";iframe.style.height = "100%";iframe.frameBorder="0";iframe.src = "https://events.eventzilla.net//widget_event?eventid=2138928651";var evntzilladiv=document.getElementById('eventzilla-iframe');evntzilladiv.appendChild(iframe);};</script>
+        
+                    </div><!--boxwhite--> 
+</div><!--wrapper--> 
+      
+        </div>
+
+   
+    
+    <h3><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <span>Venue</span></h3>
+  <div>
+   
+   <div class="wrapper">    
+    <div class="box white">
+        
+        <h3>Address</h3>
+    <p>Entrance via the Alec Stewart Gate (bottom of Harleyford Road)<br>
+    The Kia Oval <br>
+    Kennington, London<br>
+    SE11 5SS</p>
+        
+        
+ <a href="/img/kia_map.png" class="swipebox" title="Map to the Kia Oval">
+      <img class="map" src="/img/kia_map.png" alt="Map to the Kia Oval"/>
+    </a>
+        
+    <h3>Travelling by Tube</h3>
+     <p>It is a 10 minute walk from Vauxhall station to the Oval (Kings Cross/St Pancras or Euston approx 10-12 minutes to Vauxhall station via the Victoria line). The Oval can also be reached via the Northern Line and the main entrance to the ground is 100 metres on your left on leaving the station.</p>
+
+    <h3>Travelling by Bus</h3>
+    <p>Route numbers 36, 185 and 436 stop outside the ground and other local services stop around the corner. The following buses set down within a 5-minute walk of the ground: 3, 59, 109, 133, 159, 333.</p>
+            </div><!--boxwhite--> 
+</div><!--wrapper--> 
+      
+
+      
+<div class="wrapper">
+         <p>Registration will take place in the Ashes Suite on the first floor of the Oval. Don’t forget to bring your Home Office pass. We don’t want you to wear it but you must show it when you arrive on the day. If you don’t bring it you may not be able to take part.</p>
+    
+  <!-- <a href="/img/map.png" class="swipebox" title="Floorplan for the Kia Oval">
+      <img class="map" src="img/map.png" alt="map of the Kia Oval showing which rooms are on which floor"/>
+      <p>(click the image to enlarge)</p>
+    </a> -->
+
+     </div><!--wrapper--> 
+
+  </div><!-- end venue-->
+
+
+
+ <h3><i class="fa fa-info-circle fa-lg" aria-hidden="true"></i> <span>About DD<div class="caps-change">a</div>T Together</span></h3>
+  <div>
+
+<div class="wrapper">
+   <p>This is our fourth DDaT Together event and will be hosted by Pete Rose, interim Chief Digital, Data and Technology (DDaT) Officer. Book your place to find out more about DDaT’s vision and future plans as well as hearing from Jacqueline de Rojas, the President of techUK and Tom Read, Chief Digital and Information Officer at the Ministry of Justice.</p>   
+ 
+<p>This event is open to everyone in DDaT but space is limited so places will be allocated on a first-come-first-served basis. You must register via this app to secure your place to attend the event.</p>
+ 
+<p>Don’t forget to bring your Home Office pass. We don’t want you to wear it but you must show it when you arrive at The Kia Oval. If you don’t bring it, you may not be able to take part for security reasons.</p>
+ 
+<p>Any questions? Ask the <a href="mailto:hoddatcommunications@homeoffice.gsi.gov.uk">DDaT communications team</a> and we will reply promptly.</p>
+
+     </div><!--wrapper--> 
+
+  </div><!-- end info-->
+
+ <h3><i class="fa fa-bolt fa-lg" aria-hidden="true"></i> <span class="two-lines">Lightning Talks – the floor is yours <div class="font-change">@</div> DD<div class="caps-change">a</div>T Together</span></h3>
+  <div>
+
+<div class="wrapper">
+    <p>The ever-popular lightning talks will be back and we want to hear from you with your suggested topic. This should preferably align with the overall focus of the day.</p>
+ 
+ <p>The floor will be yours - be brave and share your thoughts. Presenting in front of 350 of your colleagues is a great development opportunity! You can talk with or without slides, you can speak as an individual or on behalf of a team, and you can present solo or as a group – just remember to cover your topic in three minutes!</p>
+    
+    <p>Please contact <a href="mailto:ann.blundy@homeoffice.gsi.gov.uk">Ann Blundy</a> if you would like to take part or discuss your ideas further.</p>
+ 
+    </div><!--wrapper--> 
+  </div><!-- end info-->
+
+
+<h3><i class="fa fa-commenting-o fa-lg" aria-hidden="true"></i> <span class="two-lines">Join the conversation <div class="font-change">@</div> DD<div class="caps-change">a</div>T Together</span></h3>
+  <div>
+
+<div class="wrapper">
+   <p>We want to hear your views.</p>
+ 
+    <p>During the event, we are using Sli.do again. The Sli.do code is #E061.</p> 
+    
+    <p>If you are unable to attend the event or wish to pose a question anonymously before the day you can raise it <a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/">here</a>. Please ensure all questions are posed by 5pm on 2 October as the responses will not be monitored on the day.</p>
+    
+    <p>Please contact the <a href="mailto:hoddatcommunications@homeoffice.gsi.gov.uk">DDaT communications team</a> if there is a topic or issue you would like to be explored at DDAT Together.</p>
+
+     </div><!--wrapper--> 
+  </div><!-- end info-->
+
+<h3><i class="fa fa-wifi fa-lg" aria-hidden="true"></i><span class="two-lines"> Wifi details and other useful information</span></h3>
+  <div>
+
+<div class="wrapper">
+    <h3>Registration and refreshments</h3>
+
+<p>Registration will take place in the Ashes Suite which is situated on the first floor from 9.30am.  If your travel arrangements mean you need to arrive a little early you will be able to work and/or network with other DDaT colleagues in the Pakistan Room on the first floor until registration commences at 9.30am.  Our DDaT Together volunteers will be on hand to welcome you and to direct you to the Pakistan Room or the Ashes Suite (both rooms are accessed via the lifts or stairs on your right as you enter the Kia Oval reception).</p>
+    
+<p>Please ensure you bring your Home Office pass with you. You will need to show this when you arrive/at registration for security reasons.</p>
+      
+<p>Lunch is included in the conference package and will also be served in the Ashes Suite. The Oval will provide sandwiches and seasonal salads which will include vegetarian and vegan options.</p>
+    
+<p>Please note: Any dishes known to contain nuts will be identified; however all their dishes may contain nut traces. If you have specific dietary requirements you are welcome to bring your own lunch on the day if you’d prefer.</p>
+    
+<h3>Wifi details</h3>
+<p>You can log onto the wifi whilst in the Oval using the details below:<br>
+Network: SCCC<br>
+    Password: Hobbs598</p>
+    
+<h3>Prayer room</h3>
+<p>To be confirmed on the day.</p>
+ 
+<h3>Floor plan</h3>
+     <a href="/img/floor_plan.png" class="swipebox" title="Floor plan of the Kia Oval">
+      <img class="map" src="/img/floor_plan.png" alt="Floor plan of the Kia Oval"/>
+        <p class="center"> (click to enlarge)</p>
+    </a>
+    
+    
+     </div><!--wrapper--> 
+
+
+  </div><!-- end info-->
+
+
+
+<!-- 
+
+ <h3><i class="fa fa-comments fa-lg" aria-hidden="true"></i> <span>Chat</span></h3>
+  <div>
+
+<div class="wrapper">
+  <div class="boxx">
+      <p>We use an online chat website called Slack to informally talk to each other before and during DDaT Together. You don’t have to use it and all formal communication will be done through this app. Our Slack channel is available to anyone with a gsi or digital.homeoffice email address</p>
+      
+       <h3 class="big-space">Chat using your mobile</h3>
+    <p>If you want to chat using your mobile, please install the Slack app. Available on Android, Apple and Windows phones. </p>
+      <p>Slack team: hoaway.slack.com</p>
+     <div class="button_container_first">
+    <a class="btTxt" target="_blank" href="https://itunes.apple.com/app/slack-app/id618783545?ls=1&mt=8">Slack for iOS</a>
+</div>
+     <div class="button_container">
+    <a class="btTxt" target="_blank" href="https://play.google.com/store/apps/details?id=com.Slack">Slack for Android</a>
+</div>
+     <div class="button_container">
+    <a class="btTxt" target="_blank" href="https://www.microsoft.com/en-us/store/apps/slack-beta/9nblggh1jj9h">Slack for Windows</a>
+</div>
+    </div>
+  <div class="boxx">
+     <h3>Chat using your computer</h3>
+     <p>Please note that Slack will not work on POISE Fujitsu machines.</p>
+    <p>If you wish to chat using your computer, please click the link below.</p>
+     <div class="button_container_first">
+    <a class="btTxt" target="_blank" href="https://hoaway.slack.com/">Open Slack in web browser</a>
+</div>
+    </div>
+     </div><!--wrapper 
+
+  </div><!-- end chat-->
+
+</div> <!--end info--> 
+            
+
+<?php include('footer.php')?>
