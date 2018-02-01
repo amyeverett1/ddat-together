0e870045053cf571873f3e231a4905c393155e40
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-02-01 21:29:41 +0000

    footer amend

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
         <footer>
-            <p>For the best experience using this site, we recommend you use Google Chrome as your browser.</p>
+            <p>For the best experience using this site, we recommend you use Google Chrome or Microsoft Edge as your browser.</p>
                <div class="footer_button_container">
        <a class="btTxt" href="https://ddat-together-events.herokuapp.com/"><i class="fa fa-angle-double-left fa-lg" aria-hidden="true"></i> Back to the main DDaT events site</a>
             </div>   
